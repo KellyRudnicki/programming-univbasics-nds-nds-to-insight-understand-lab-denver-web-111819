6d2935ad709ac485f67f7fbfa537f17cdd90cda6
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2019-11-10 00:49:48 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,8 +4,27 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
+  pp nds 
   nil
 end
 
 def print_first_directors_movie_titles
+  
+  directors_database
+  outer_hash = 0
+  test = []
+
+      
+    while outer_hash < directors_database.length do
+      if directors_database[outer_hash][:name].include? "Stephen Spielberg"
+        inner_hash = 0
+        #test << directors_database[outer_hash][:movies]
+        while inner_hash < directors_database[outer_hash].length do
+		    test << directors_database[outer_hash][inner_hash]
+		    inner_hash += 1
+		    end
+      end
+      outer_hash += 1
+    end
+    puts test
 end
