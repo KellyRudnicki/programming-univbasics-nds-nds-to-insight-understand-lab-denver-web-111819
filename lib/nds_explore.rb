0a30de008ffa20406ed7a30843bfcc8ad361d09a
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2019-11-10 01:35:01 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,7 +11,24 @@ end
 def print_first_directors_movie_titles
   
   directors_database
+  outer_hash = 0
+  test = []
 
-		puts directors_database[movies][title]
+      
+    while outer_hash < directors_database.length do
+      if directors_database[outer_hash][:name].include? "Stephen Spielberg"
+        inner_hash = 0
+        #test << directors_database[outer_hash][:movies]
+        while inner_hash < directors_database[outer_hash].length do
+		      test << directors_database[outer_hash][:movies][inner_hash][:title]
+		      inner_hash += 1
+		    end
+      end
+      outer_hash += 1
+    end
+    
+    puts test
+	
+end
 	
 end
